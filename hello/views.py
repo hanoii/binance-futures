2a1b03ca238ed36b3a07927093d90b273353d962
Ariel Barreiro <abarrei@gmail.com>
2021-06-15 01:09:04 -0300

    Improve stop view

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import json
+import traceback
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
@@ -9,13 +11,18 @@ from binance_f.constant.test import *
 from binance_f.base.printobject import *
 from binance_f.model.constant import *
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    binance = {'api_key':os.getenv('BINANCE_API_KEY'), 'secret_key': os.getenv('BINANCE_API_SECRET')}
+def binance_request_client():
+    binance = {'api_key':os.environ['BINANCE_API_KEY'], 'secret_key': os.environ['BINANCE_API_SECRET']}
     if os.getenv('BINANCE_TESTNET') == 'true':
         binance['url'] = 'https://testnet.binancefuture.com'
     request_client = RequestClient(**binance)
+    return request_client
+
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    request_client = binance_request_client()
     result = request_client.get_account_information_v2()
     context = {'binance': result}
     return render(request, "index.html", context)
@@ -23,9 +30,58 @@ def index(request):
 
 @require_http_methods(["POST"])
 def stop(request):
+    response = {}
     try:
-        mydata = json.loads(request.body.decode("utf-8"))
-        return JsonResponse(mydata)
+        quantity = 0
+        payload = json.loads(request.body.decode("utf-8"))
+
+        if not 'symbol' in payload:
+            raise ValueError("symbol not in payload.")
+
+        if not 'operation' in payload:
+            raise ValueError("operation not in payload.")
+
+        if payload['operation'] not in ['sell', 'buy']:
+            raise ValueError("operation must be either 'buy' or 'sell'.")
+
+
+        response['payload'] = payload
+        request_client = binance_request_client()
+
+        result = request_client.get_exchange_information()
+        found = False
+        for symbol in result.symbols:
+            if symbol.symbol == payload['symbol']:
+                found = True
+                precision = symbol.quantityPrecision
+
+        if not found:
+            raise ValueError("Symbol " + payload['symbol'] + " not found.")
+
+        if not 'quantity' in payload:
+            result = request_client.get_position_v2()
+            for position in result:
+                if position.symbol == payload['symbol']:
+                    quantity = position.positionAmt
+                    response['quantity'] = quantity
+        else:
+            quantity = payload['quantity']
+            response['quantity'] = quantity
+
+        if quantity > 0:
+            quantity = ("{:." + str(precision) + "f}").format(quantity)
+            order = {
+                'symbol': payload['symbol'],
+                'ordertype': OrderType.MARKET,
+                'quantity': quantity,
+                'side': OrderSide.SELL
+            }
+            response['order'] = order
+            result = request_client.post_order(**order)
+        else:
+            raise ValueError("quantity is 0.")
+        return JsonResponse({'success': 'true', 'context': response})
     except:
-        return JsonResponse({'success': 'false', 'error': 'Invalid payload.'})
+        var = traceback.format_exc()
+        return JsonResponse({'success': 'false', 'traceback': var.splitlines()})
 
